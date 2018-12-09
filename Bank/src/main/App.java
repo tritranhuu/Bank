f585cd91cd79cee5a970cff463eaba86064434eb
Huu Tri Tran <tritranhuu123@gmail.com>
2018-12-10 01:19:00 +0700

    fix

--- Bank/src/main/App.java
+++ Bank/src/main/App.java
@@ -2,17 +2,38 @@ package main;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 import gui.GUI;
 
 public class App {
-
+	
+	
+	
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket client = new Socket("127.0.0.1", 8888);
-		
+		//Object[] buttons = {"Connect!"};
+		//String ip = JOptionPane.showInputDialog(null, "Enter host IP address", "MyInternetBanking", JOptionPane.PLAIN_MESSAGE);
+		String ip = JOptionPane.showInputDialog(null, "Enter host IP address", "MyInternetBanking", JOptionPane.PLAIN_MESSAGE);
+		if(ip!=null) {
+			try {
+			Socket client = new Socket(ip, 8888);
 		GUI gui = new GUI(client);
 		GUI.showMain2();
 		//gui.exit();
+		} catch(SocketException e) {
+			JOptionPane.showMessageDialog(null, "Cannot connect to that IP!!!", "Connection Error", JOptionPane.ERROR_MESSAGE);
+		}
+		
 	}
+		
+	}
+	
 }
